4c52c53184ee7fbc3f326f49e822697a1f8fa030
Cleiton Feitosa <cleiton.costa@outlook.com.br>
2021-03-19 09:38:45 -0300

    Calculadora

---
+++ Calculadora.cs
@@ -0,0 +1,25 @@
+﻿class Calculadora
+{
+
+    public string Soma(double numero1, double numero2)
+    {
+        double resultado = numero1 + numero2;
+        return ("                      O resultado da sua soma é = " + resultado);
+    }
+    public string Subtracao(double numero1, double numero2)
+    {
+        double resultado = numero1 - numero2;
+        return ("                    O resultado da sua subtração é = " + resultado);
+    }
+    public string Divisao(double numero1, double numero2)
+    {
+        double resultado = numero1 / numero2;
+        return ("                      O resultado da sua divisão é = " + resultado);
+    }
+    public string Multiplicacao(double numero1, double numero2)
+    {
+        double resultado = numero1 * numero2;
+        return ("                   O resultado da sua multiplicação é = " + resultado);
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string operacao;
+            double numero1, numero2;
+            bool verifc = true;
+            Calculadora a = new Calculadora();
+
+            Console.WriteLine("                         Bem vindo a Calculadora");
+            Console.WriteLine("Aqui você pode fazer calculos de Soma, Subtração, Multiplacação e Divisão");
+            Console.WriteLine("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+            Console.WriteLine("                        Digite o primeiro Número");
+            numero1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("                   Digite o sinal da operação x,/,-,+");
+            operacao = Console.ReadLine();
+            Console.WriteLine("                        Digite o segundo Número");
+            numero2 = double.Parse(Console.ReadLine());
+
+            do
+            {
+                switch (operacao)
+                {
+                    case "x":
+                        Console.WriteLine(a.Multiplicacao(numero1, numero2));
+                        verifc = false;
+                        break;
+                    case "/":
+                        Console.WriteLine(a.Divisao(numero1, numero2));
+                        verifc = false;
+                        break;
+                    case "-":
+                        Console.WriteLine(a.Subtracao(numero1, numero2));
+                        verifc = false;
+                        break;
+                    case "+":
+                        Console.WriteLine(a.Soma(numero1, numero2));
+                        verifc = false;
+                        break;
+                    default:
+                        Console.WriteLine("                    ERRO DE OPERAÇÂO TENTE NOVAMENTE");
+                        Console.WriteLine("                   Digite o sinal da operação x,/,-,+");
+                        operacao = Console.ReadLine();
+                        verifc = true;
+                        break;
+                }
+
+            } while (verifc == true);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
